c6392c5bf5ba3a8de31d8d9827c809bfe6dfaa46
David <quachddavid@gmail.com>
2022-05-17 22:46:25 +1000

    added links for user sign in, sign up, sign out

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+david = User.create(email: 'david@qmail.com', password: 'apple322')
+bob = User.create(email: 'bob@qmail.com', password: 'apple322')
+puts "Users: #{User.count}"
